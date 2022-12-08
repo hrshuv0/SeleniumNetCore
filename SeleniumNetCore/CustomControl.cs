d127a459db3bf25d2fed4953f7b0f1c3a9bf2e3b
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-08 14:47:34 +0600

    Add Custom control

---
+++ SeleniumNetCore/CustomControl.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumNetCore;
+
+public class CustomControl : DriverHelper
+{
+    public static void ComboBox(string controlName, string value)
+    {
+        IWebElement comboControll = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+        comboControll.Clear();
+        comboControll.SendKeys(value);
+        Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+    }
+}
--- SeleniumNetCore/UnitTest1.cs
+++ SeleniumNetCore/UnitTest1.cs
@@ -1,40 +1,36 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace SeleniumNetCore
+namespace SeleniumNetCore;
+
+public class Tests : DriverHelper
 {
-    public class Tests
+    [SetUp]
+    public void Setup()
     {
-        public IWebDriver Driver;
+        Console.WriteLine("Setup");
 
-        [SetUp]
-        public void Setup()
-        {
-            Console.WriteLine("Setup");
+        ChromeOptions chromeOptions = new ChromeOptions();
 
-            ChromeOptions chromeOptions = new ChromeOptions();
+        Driver = new ChromeDriver();
+    }
 
-            Driver = new ChromeDriver();
-        }
+    [Test]
+    public void Test1()
+    {
+        Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
 
-        [Test]
-        public void Test1()
-        {
-            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+        // Set input field text
+        Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
 
-            // Set input field text
-            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+        // Click on ckeckbox - with no id
+        Driver.FindElement(by: By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
 
-            // Click on ckeckbox - with no id
-            Driver.FindElement(by: By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+        CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
 
-            IWebElement comboControll = Driver.FindElement(By.XPath("//input[@id='ContentPlaceHolder1_AllMealsCombo-awed']"));
-            comboControll.Clear();
-            comboControll.SendKeys("Almond");
-            Driver.FindElement(By.XPath("//div[@id='ContentPlaceHolder1_AllMealsCombo-dropmenu']//li[text()='Almond']")).Click();
+        
 
-            Console.WriteLine("Test1");
-            Assert.Pass();
-        }
+        Console.WriteLine("Test1");
+        Assert.Pass();
     }
 }
