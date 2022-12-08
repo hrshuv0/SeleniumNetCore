45bc7ce8904123b5b2719cfc008496e582db9b97
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-08 12:18:49 +0600

    Add project files.

---
+++ SeleniumNetCore/UnitTest1.cs
@@ -0,0 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumNetCore
+{
+    public class Tests
+    {
+        public IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+
+            // Set input field text
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+
+            // Click on ckeckbox - with no id
+            Driver.FindElement(by: By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
