0b9dd0c8443b844568e9e3bccbdeb87ba46dc58d
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-08 15:09:42 +0600

    Add Dropdown select

--- SeleniumNetCore/UnitTest1.cs
+++ SeleniumNetCore/UnitTest1.cs
@@ -20,12 +20,18 @@ public class Tests : DriverHelper
     {
         Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
 
-        // Set input field text
-        Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+        //// set input field text
+        //// Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
 
-        // Click on ckeckbox - with no id
-        Driver.FindElement(by: By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+        //CustomControl.EnterText(Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")), "Tomato");
 
+        //// Click on ckeckbox - with no id
+        //// Driver.FindElement(by: By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+        //CustomControl.Click(Driver.FindElement(by: By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")));
+
+        CustomControl.SelectByText(Driver.FindElement(By.Id("ContentPlaceHolder1_Add1-awed")), "Cauliflower");
+        
         CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
 
         
